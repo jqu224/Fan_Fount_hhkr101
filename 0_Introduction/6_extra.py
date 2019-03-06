7816798d70bc0974ecee0cdf468feb173460849c
Jcq <q.roggee@gmail.com>
2019-03-05 20:26:32 -0800

    Update 6_extra.py

--- 0_Introduction/6_extra.py
+++ 0_Introduction/6_extra.py
@@ -1,8 +1,10 @@
-["a", "b", "bsss"] is a list of element, let print it in succession
+# ["a", "b", "bsss"] is a list of element, let print it in succession
+# 把一堆字符cahr: a b c d，想刚才123一样连续打印出来…
+# char和数字123一样，是基本element
 
-
+# answer:
 "".join(["a", "b", "b"])
 
 
-output jiu shi:  
+# output jiu shi:  
 >>>   'abb'
