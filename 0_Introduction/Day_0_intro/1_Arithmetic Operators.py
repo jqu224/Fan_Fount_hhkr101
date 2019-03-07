33e96d1e47134030a3b3b96b00a4f4f9d2a37a4f
Jcq <q.roggee@gmail.com>
2019-03-06 21:15:10 -0800

    Add files via upload

---
+++ 0_Introduction/Day_0_intro/1_Arithmetic Operators.py
@@ -0,0 +1,8 @@
+# if __name__ == "main": 这一个月都不用管，是ensure以下内容为main function的内容
+# BTW __name__ means immutable (cant change) always works with main function or core value that you shouldnt touch very oftern...
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
---
+++ 0_Introduction/Day_0_intro/3_Loops.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    #  in the case of range(a,b,c) if range(b) i.e. from 0.to.b-1
+    for i in range(n):
+        print(i ** 2)
