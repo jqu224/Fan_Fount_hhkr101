90d8fb9775129dac2bb08da2356e01efe0fe5f40
Jcq <q.roggee@gmail.com>
2019-03-07 23:12:07 -0800

    Create 1_split_join.py

---
+++ 2_Strings/1_split_join.py
@@ -0,0 +1,8 @@
+def split_and_join(line):
+    # write your code here
+    temp = line.split()
+    return "-".join(temp)
+    # this should be easy, so no detailed breakdown
+    # if lost, refer to last two days folder: 0_into and 1_basics
+
+if __name__ == '__main__':
