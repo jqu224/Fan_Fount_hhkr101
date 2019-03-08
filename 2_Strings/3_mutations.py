915fc768821d43de8a98e2a92297af2973ed0e58
Jcq <q.roggee@gmail.com>
2019-03-07 23:12:58 -0800

    Create 3_mutations.py

---
+++ 2_Strings/3_mutations.py
@@ -0,0 +1,6 @@
+def mutate_string(string, position, character):
+
+    return string[:position] + character + string[position+1:]
+    #  easy one, if you learned from the previous questions of strings
+
+if __name__ == '__main__':
