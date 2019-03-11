42dbc77ec6868f8bbcc239fd78980992c2e2a5d9
Jcq <q.roggee@gmail.com>
2019-03-10 21:04:38 -0700

    Update 3_mutations.py

--- 2_Strings/3_mutations.py
+++ 2_Strings/3_mutations.py
@@ -2,5 +2,16 @@ def mutate_string(string, position, character):
 
     return string[:position] + character + string[position+1:]
     #  easy one, if you learned from the previous questions of strings
+    
+    def mutate_string(string, *position):
+    # print(position[0], position[1])
+    return string[:position[0]] + position[1] + string[position[0]+1:]
+    
+    #  easy one, if you learned from the previous questions of strings
 
 if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+     
