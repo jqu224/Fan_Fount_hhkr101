a5bf00b91ce71c8cfeb3bdabcc2e827178465fd3
Jcq <q.roggee@gmail.com>
2019-03-10 21:36:09 -0700

    Update 3_mutations.py

--- 2_Strings/3_mutations.py
+++ 2_Strings/3_mutations.py
@@ -29,9 +29,20 @@ def mutate_string(string, *position):
 
     那么这相当于，string, *position = s, int(i), c 所以：string = c 而 position = 【int(i)   c】
     提这是trick因为有时候不知道传入 defined function 的 argument 的长度
-
+    
+    
+    用*号,就把 int(i) 给了position【0】，把 c 赋予了 position【1】，所以上面的 code 也是能 pass the tests 的
+    s_new = mutate_string(“abracadabra ”,  5,  “k”)对吗？
+    def mutate_string(string, *position): 
+        这里，string == “abracadabra ” 
+        而 position == [5,  “k”] 了
+        
+        
     就和 a, *b = list( [1 2 3 4 5 6] )一样呀，a == 1, b == [2 3 4 5 6]
 
+    
+    
+    
 2，list 可以由不同的type组成 
 
     譬如 int char string float 也可以list里套着别的【list【】】 
@@ -49,8 +60,4 @@ def mutate_string(string, *position):
 
 
 
-    用*号,就把 int(i) 给了position【0】，把 c 赋予了 position【1】，所以上面的 code 也是能 pass the tests 的
-    s_new = mutate_string(“abracadabra ”,  5,  “k”)对吗？
-    def mutate_string(string, *position): 
-        这里，string == “abracadabra ” 
-        而 position == [5,  “k”] 了
+
