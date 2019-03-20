69ff2eb020ddf75fdcd1e3ea1f6f9e6a4eb93480
Jcq <q.roggee@gmail.com>
2019-03-19 20:35:30 -0700

    Update 2_Wrap.py

--- 3_string1/2_Wrap.py
+++ 3_string1/2_Wrap.py
@@ -1,12 +1,20 @@
 import textwrap
 
 def wrap(string, max_width):
+    
+#  solution a
     temp_list = list(string)
     # try for i in range(1, len(string)): and click run code
     for i in range(len(string)+1,1,-1):
         if i % max_width == 0:
             temp_list.insert(i, "\n")
     return "".join(temp_list)
+
+#  solution b 
+    answer = [] 
+    for i in range(0,len(string),max_width): 
+        answer.append(string[i:i+max_width])
+    return "\n".join(answer)
 
 if __name__ == '__main__':
     string, max_width = input(), int(input())
