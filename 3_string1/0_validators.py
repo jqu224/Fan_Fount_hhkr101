e360d02504a9f3e04aa419c80767a100fec52202
Jcq <q.roggee@gmail.com>
2019-03-20 18:52:31 -0700

    Update 0_validators.py

--- 3_string1/0_validators.py
+++ 3_string1/0_validators.py
@@ -59,4 +59,27 @@ for把 c in str 变成了list，
     print(bool(anyUpper))
 
 #  remember the most important thing is to do the lian-lian-look
-#  dont you ever make a silly mistake while typing the if statements
+#  dont you ever make a silly mistake while typing the if statements pls
+
+
+上次的🌰，只是告诉你要小心，
+很多时候，return的答案不对，
+是因为code的时候，if case的<>=这些边界条件设计不对 
+
+但是呢，有时候，这个if里面的操作很复杂，
+就可以把这个anyvalue判断放在if后面用and连接，
+这样就不必做重复操作了 像这样的话，最好还是让if多判断一下，
+只有在这个char是alpha且没有翻过anyalphaetidcal牌子的情况下，
+``` 
+        if s[i].isdigit() and anyDig == 0:
+            anyDig = 1
+            anyDi1g = 1
+            anyDig2 = 1
+            anyDig3 = 1
+            anyDi4g = 1
+            anyDig6 = 1
+```
+我们才进去做这一长串的any = 1赋值 
+这里的重复操作是anylower = 1，也就是对把anylower变成true，这里比较简单跑一次也不碍事，所以and后面的可以不写 换句话说： 这个if我进去玩过了，这次是我的第二次路过这个if isdigit == true，就不进去了
+
+
