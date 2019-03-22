23fce1f7c93536050ca238169a07466cd6b2f3be
Jcq <q.roggee@gmail.com>
2019-03-21 19:25:25 -0700

    Update 1_Capitalize.py

--- 3_String2/1_Capitalize.py
+++ 3_String2/1_Capitalize.py
@@ -30,9 +30,7 @@ def solve(s):
             counter = 0
         elif counter == 0:
             temp[i] = temp[i].upper()
-            counter += 1
-        else:
-            counter += 1
+            counter = 1 
     return("".join(temp))
 
 if __name__ == '__main__':
@@ -45,3 +43,17 @@ if __name__ == '__main__':
     fptr.write(result + '\n')
 
     fptr.close()
+
+    
+    
++++++++++++++++++++++     NOTE      NOTE       NOTE     ++++++++++++++++++++++
+    
+    
+counter 用来记录当前是不是 单个word 的第一个字母，如果是，把这个字母upper case，然后counter +1
+碰到空格，counter变回 0 
+譬如 “ac wf”
+for i,a in enumerate(list_temp): i是index数字，a是一个个char
+    i == 0 counter == 0 则 list[0] = list[0].upper() -> "a" 变成"A" 然后 counter +1
+    i == 1 counter == 1 此时 char == "c" 说明这时候不是首字母，不要管他，else可写可不写。
+    i == 2 counter == 2 此时 char == " " 进入 if char == " " 更新 counter = 0
+    i == 3 counter == 0 此时 char == "w" 进入 elif counter == 0 更新  list[3] = list[3].upper() -> "w" 变成"W" 
