cece631ebd52c304941a7a03ecddb5affa0b5bbb
Jcq <q.roggee@gmail.com>
2019-03-21 20:03:03 -0700

    Update 3_merge_this.py

--- 3_String2/3_merge_this.py
+++ 3_String2/3_merge_this.py
@@ -15,15 +15,20 @@ def merge_the_tools(string, k):
     if k == 1:
         print("\n".join(list(string)))
     else:    
-        for i in range(k):
+        # there will be len / k lines since n is always a multiple of k 
+        for i in range(len(string)//k):
+            # chop off [0:k] [k:2k] [2k:3k] ....
             temp_list = list(string[i*k:i*k+k])
+            # create a temp string for unique char in [0:k] ... and so on 
             temp_set = ""
+            # loop through the [0:k] lists
             for x in temp_list:
+                # if current char is not in the temp string 
+                # then append it to the end of temp string
                 if x not in temp_set:
                     temp_set += x
             print(temp_set)
-
-    # print("\n".join(ult_list))
+ 
 
 
 
