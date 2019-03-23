9fd5816b779da9dae89946c5774b09fb51e38d94
Jcq <q.roggee@gmail.com>
2019-03-22 23:29:03 -0700

    Update 0_intro_2_set.py

--- 4_set/0_intro_2_set.py
+++ 4_set/0_intro_2_set.py
@@ -7,3 +7,10 @@ if __name__ == '__main__':
     arr = list(map(int, input().split()))
     result = average(arr)
     print(result)
+
+    
+    
+char是string的最小单位
+“11”这个string 有两个char分别是“1”和“1”
+“13”这个string 有两个char分别是“3”和“1”
+“ab”这个string 有两个char分别是“a”和“b”
