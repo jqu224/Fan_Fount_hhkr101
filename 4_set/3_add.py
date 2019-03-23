09a9a0d0d9b257834b35c0987fd74636a8ad9c5a
Jcq <q.roggee@gmail.com>
2019-03-22 23:35:06 -0700

    Update 3_add.py

--- 4_set/3_add.py
+++ 4_set/3_add.py
@@ -6,3 +6,11 @@ for i in range(n):
     country_name = str(input())
     set_country.add(country_name)
 print(len(set_country))
+
+
+# >>> a = input()
+# 这里python会让我一直输入，知道出现回车为止：234234   sdf9sd99923 99423 =-34= -=-=
+# >>> a
+# '234234   sdf9sd99923 99423 =-34= -=-= '
+
+
