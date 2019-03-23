51e6b9cf6ad9c1e1b0f6e006f0ce3154f3d79538
Jcq <q.roggee@gmail.com>
2019-03-22 23:29:27 -0700

    Update 2_Symmetric_Difference.py

--- 4_set/2_Symmetric_Difference.py
+++ 4_set/2_Symmetric_Difference.py
@@ -14,6 +14,9 @@ diff_0 = list(list_a.difference(list_b).union(list_b.difference(list_a)))
 diff_0.sort() 
 print("\n".join(map(str, diff_0)))
 
+# listen 2 有说, a = sorted(a) 等同于 a.sort()
+
+
 # solution b
 # diff = list(list_a.union(list_b) - list_a.intersection(list_b))
 # diff.sort() 
