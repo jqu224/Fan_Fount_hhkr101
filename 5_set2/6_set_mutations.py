d1ed79bb67fe2cc23e5c8015f98c0a7d34509050
Jcq <q.roggee@gmail.com>
2019-03-25 20:23:12 -0700

    Create 6_set_mutations.py

---
+++ 5_set2/6_set_mutations.py
@@ -0,0 +1,29 @@
+https://www.hackerrank.com/challenges/py-set-mutations/problem
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+list_a = set(map(int, input().split()))
+
+for _ in range(int(input())):
+    cmmd, *num = input().split()
+    list_b = set(map(int, input().split()))
+    
+    if len(num) == 0:
+        pass
+    else:
+        num = int(num[0])
+
+    if cmmd == "update":
+        list_a |= list_b 
+    elif cmmd == "intersection_update": 
+        list_a &= list_b
+    elif cmmd == "symmetric_difference_update": 
+        list_a ^= list_b 
+    elif cmmd == "difference_update":
+        list_a -= list_b
+
+print(sum(list_a))
+
+
