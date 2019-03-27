a0cbdfe2625e9e16b82f9cb802e4400697933308
Jcq <q.roggee@gmail.com>
2019-03-26 19:08:27 -0700

    Create 1_mod.py

---
+++ 6_math/1_mod.py
@@ -0,0 +1,16 @@
+https://www.hackerrank.com/challenges/python-mod-divmod/problem
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+div = int(input())
+print(n // div)
+print(n % div)
+print(divmod(n,div))
+
+# n = int(input())
+# div = int(input())
+# a, b = divmod(n,div)
+# print(a)
+# print(b)
+# print(divmod(n,div))
