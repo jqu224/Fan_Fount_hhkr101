90ce6cd34afad0075de02309e4d3dce639bdd36b
Jcq <q.roggee@gmail.com>
2019-03-26 19:17:41 -0700

    Create 5_Triangle.py

---
+++ 6_math/5_Triangle.py
@@ -0,0 +1,3 @@
+https://www.hackerrank.com/challenges/triangle-quest-2/problem
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print([1, 121, 12321, 1234321, 123454321, 12345654321, 1234567654321, 123456787654321, 12345678987654321, 12345678910987654321][i-1])
