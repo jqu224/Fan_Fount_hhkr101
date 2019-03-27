3bd862fb999c8a271c51eb390a89a57e389e6b95
Jcq <q.roggee@gmail.com>
2019-03-26 20:32:28 -0700

    Update 0_zipped.py

--- 7_built_in/0_zipped.py
+++ 7_built_in/0_zipped.py
@@ -2,7 +2,7 @@ https://www.hackerrank.com/challenges/zipped/problem
 
 
 # # # # # # #
-# solution b 
+# solution a
 # Enter your code here. Read input from STDIN. Print output to STDOUT
  
 
@@ -10,14 +10,25 @@ student, tests = map(int, input().split())
 int_list = []
 for _ in range(tests):
     int_list += [list(map(float, input().split()))]
-
+  
 for a in zip(*int_list):
     print(sum(a) / tests)
+  
+  
+# 星号是用来unpack的，譬如 
+# int_list = [ [list_a], [list_b], [list_c] ]
+# *int_list = [list_a] [list_b] [list_c]
+# 这样才能传入：zip()，
+# zip()是把()内的所有的list的 第一个map到一个tuple或者list里， tuple还是list由你选择
+# 第 2 个map到一个tuple或者list里，
+# 第 3 个map到一个tuple或者list里...
+# 譬如 list(zip( [1, 11, 111], [2, 22, 222] [3, 33, 333] )) => [（1,2,3）, （11,22,33）,（111,222,333）]
+# 又譬如 tuple(zip( [1, 11, 111], [2, 22, 222] [3, 33, 333] )) => （（1,2,3）,（11,22,33）,（111,222,333））
 
 
 
 # # # # # # 
-# solution a
+# solution a.2
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
 student, tests = map(int, input().split())
