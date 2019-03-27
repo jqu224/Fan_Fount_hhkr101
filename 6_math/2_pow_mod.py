5d9edc72848a712cdf7248600dba916a29ffe81b
Jcq <q.roggee@gmail.com>
2019-03-26 19:11:45 -0700

    Create 2_pow_mod.py

---
+++ 6_math/2_pow_mod.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# a = int(input())
+# b = int(input())
+# m = int(input())
+# print(pow(a,b))  
+# print(pow(a,b,m))  
+
+# solution b
+a = int(input())
+b = int(input())
+m = int(input())
+
+output1 = a ** b
+output2 = (a ** b) % m
+print(output1)  
+print(output2)  
