c555981f3844cd04b4efb673c56407e1f081f829
Jcq <q.roggee@gmail.com>
2019-03-26 19:59:21 -0700

    Update 3_Integers.py

--- 6_math/3_Integers.py
+++ 6_math/3_Integers.py
@@ -13,6 +13,19 @@ print (pow(a,b)+pow(c,d))
 
 
 ...................................
+这个solution b是抄的，意思是 for loop执行4次，
+每次int(input)一个数，这样等号右边变成了一个tuple，里面四个int，
+这时候执行等号，把 a b c d 分别指向右边这tuple里的第1234个数
+
+
+如果是a=(int(input()) for I in range(4))，
+那么是先执行右侧，先take 4个int，放到()tuple里，
+此刻这个tuple还没名字，最後执行等号，a指向这个无名tuple
+
+a=(int(input()) for I in range(4)) ==== a=(int(input()) for _ in range(4)) 
+you can use [i] [_] or [m] [name] [waht] [anything] just dont use the same name as [a]
+
+
 
 ⛵️：is this okay?
 for _ in range(4):
@@ -28,3 +41,10 @@ a = []
 for _ in range(4):
     a += [int(input())]
 用的时候call by a[0] a[1] a[2] a[3]就好了
+
+
+for只能和前几天做的一样，放在式子上方，或者今天这样放在后侧，
+2者 没有区别，建议最好都放在上方外面，不要和等式挤在one line 里
+
+a, b, c, d = (1,2,3,4) means unpack the right hand side 
+and map them to a b c d these 4 variables. NOTE! must be same length
