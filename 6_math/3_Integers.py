b31b7e19246e474295655d084ba2dcbaad0a2148
Jcq <q.roggee@gmail.com>
2019-03-26 19:14:04 -0700

    Update 3_Integers.py

--- 6_math/3_Integers.py
+++ 6_math/3_Integers.py
@@ -1,8 +1,13 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
+# solution a
 a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
 
 print(a**b + c**d)
+
+# solution b
+a,b,c,d = (int(input()) for _ in range(4))
+print (pow(a,b)+pow(c,d))
