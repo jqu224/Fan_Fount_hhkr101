c792c4bb5bdf521019b9b58ef823cee0874d2e1f
Jcq <q.roggee@gmail.com>
2019-03-27 20:43:44 -0700

    Create 4_ginsort.py

---
+++ 7_built_in/4_ginsort.py
@@ -0,0 +1,21 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+string = input()
+low = ""
+upp = ""
+odd = ""
+eve = ""
+for i in string:
+    if i.islower():
+        low += i
+    elif i.isupper():
+        upp += i
+    elif int(i) % 2 == 1:
+        odd += i
+    elif int(i) % 2 == 0:
+        eve += i
+low = sorted(list(low))
+upp = sorted(list(upp))
+odd = sorted(list(odd))
+eve = sorted(list(eve))
+print("".join([*low,*upp,*odd,*eve]))
