4b2fac03d4d7b8379ea06b3a5d3f4b1e17df4767
Jcq <q.roggee@gmail.com>
2019-03-27 18:50:22 -0700

    Create 1_input.py

---
+++ 7_built_in/1_input.py
@@ -0,0 +1,21 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x, ans = map(int, input().split())
+poly = input()
+
+if eval(poly) == ans:
+    print(True)
+else:
+    print(False)
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x, ans = map(int, input().split()) 
+
+if eval(input()) == ans:
+    print(True)
+else:
+    print(False)
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x, ans = map(int, input().split()) 
+print(True if eval(input()) == ans else False)
+ 
