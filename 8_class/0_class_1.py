e5df1cf1ddf724993952e1a440c83daf095f5d1c
Jcq <q.roggee@gmail.com>
2019-03-28 21:51:27 -0700

    Update 0_class_1.py

--- 8_class/0_class_1.py
+++ 8_class/0_class_1.py
@@ -37,8 +37,44 @@ TypeError: can't mod complex numbers.
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # following is the solution 
 # # # # # # # # # # # # # # 
+
+这里的class定义了一个（类），类别的意思，和def很像，但是放在def外面，
+如果说def是一种操作，譬如def eat（a）：让a去吃饭
+class chinese（a）：表示的是定义一个object a ，而a内置的defined function包含各种callable的可能性：
+譬如
+class chinese(object):
+  def eat(a):
+    吃吃吃
+    
+
+然后初始化：
+a = chinese()
+
+你可以用
+a.eat()
+来让a吃饭了
+
+／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／
+用的时候
+#   这里呢，__xxx__ 表示xxx是一个内部函数，譬如 
+a，b = 1, 3
+事实上a和b都是int object，自带+-*/ % //法，这些都是预先写好的，（具体看下文理解）
+因此才可以写 a + b， a - b
+下面的code就是让你重新写一遍这些内置函数
+－－－－－－－－－－－－－－－－
+self 代表这个object自己，不算传入的argument
+
+# 这里init是用来初始化的，譬如 
+a = chinese(arg1)
+而class里的init有写：
+def __init__(self, waha):
+  self.waaa = “waha”
+  
+  那么，这个a就把arg1 take in成了他的waaa这个特征了。
+////////////////////////////////////////////////////
+
 class Complex(object):
-    def __init__(self, real, imaginary):
+  def __init__(self, real, imaginary):
         self.real = real
         self.imaginary = imaginary 
     def __add__(self, no):
