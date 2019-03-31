078946a73602d46eb16a0c83f21881c814f56472
Jcq <q.roggee@gmail.com>
2019-03-31 16:02:28 -0700

    Create 9_mean_car.py

---
+++ `11_numpy/9_mean_car.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+np.set_printoptions(legacy='1.13')
+
+N, M = map(int, input().split())
+_ = np.array([input().split() for i in range(N)], int)
+
+print(np.mean(_, axis=1))
+print(np.var(_, axis=0))
+print(np.std(_, axis=None))
