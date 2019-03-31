fa3b01453af290dd4591caa4bcfff2919c6e4869
Jcq <q.roggee@gmail.com>
2019-03-31 16:10:33 -0700

    Update `10_mean_var.py

--- `11_numpy/`10_mean_var.py
+++ `11_numpy/`10_mean_var.py
@@ -2,7 +2,8 @@ https://www.hackerrank.com/challenges/np-mean-var-and-std/problem
   
 import numpy as np
 
-np.set_printoptions(legacy='1.13')
+# np.set_printoptions(legacy='1.13')
+np.set_printoptions(sign=' ')
 
 N, M = map(int, input().split())
 _ = np.array([input().split() for i in range(N)], int)
