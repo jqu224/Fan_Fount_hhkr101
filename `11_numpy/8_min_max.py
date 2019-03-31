69549731c0f188fe974c8567723c45aab4b34d79
Jcq <q.roggee@gmail.com>
2019-03-31 16:01:07 -0700

    Update 8_min_max.py

--- `11_numpy/8_min_max.py
+++ `11_numpy/8_min_max.py
@@ -1,7 +1,13 @@
 import numpy as np
-The min along axis  =  
-The max of  = 
+
+# The min along axis=1 => gives a vector: a vector is a 1d array  
+# The max of vector => single value
 n, m = map(int, input().split())
 a = np.array([input().split() for _ in range(N)],int)
-print(np.max(np.min(a, axis=1), axis=0))
 
+# solution a
+vec = np.min(a, axis=1)
+print(np.max(vec, axis=0))
+
+# solution b
+print(np.max(np.min(a, axis=1), axis=0))
