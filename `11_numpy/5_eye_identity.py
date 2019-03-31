55761a32f2aa31e965d5f9ef206315530dbe7305
Jcq <q.roggee@gmail.com>
2019-03-31 15:38:08 -0700

    Create 5_eye_identity.py

---
+++ `11_numpy/5_eye_identity.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# # # # # GOAL:
+# take in the N, M values
+# print a NxM (pronounced as N by M) eye matrix
+
+# hackerrank is silly, it's using an old print_option 
+# which adds one space char in front of each [cell] in the matrix
+np.set_printoptions(legacy='1.13')
+
+n, m = map(int, input().split())
+# solution a
+if n == m:
+    print(np.identity(n))
+else: 
+    print(np.eye(n, m, k = 1))
+
+
+# solution b
+# without np.set_printoptions(legacy='1.13')
+print(str(np.eye(*map(int,input().split()))).replace('1',' 1').replace('0',' 0'))
