80a7525f95a6e65c9a6802926c14f24fc09bb623
Jcq <q.roggee@gmail.com>
2019-03-31 16:26:42 -0700

    Update 1_reshape.py

--- `11_numpy/1_reshape.py
+++ `11_numpy/1_reshape.py
@@ -19,3 +19,24 @@ cols, = a.shape
 
 a_reshaped = np.reshape(a,(3,cols//3))
 print(a_reshaped)
+
+
+
+=======================================================
+注意这个逗号，
+>>> a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]).shape
+>>> a
+(9,)
+>>> a, = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]).shape
+>>> a
+9
+
+>>> a, = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]).shape 注意a后面的逗号，要这样才能脱掉括号
+>>> a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]).shape[0]
+a both == 9
+
+这是一个特例。
+1d是一个 n by 1 的array，
+她的 shape return是 number 和空。
+
+2d 的 shape return 是 row 和 column
